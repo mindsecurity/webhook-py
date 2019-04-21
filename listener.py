68a9db8883e33f4db1b67c83823bc8b6b8d5ea93
Marcos Tolosa <marcostolosa@protonmail.com>
2019-04-20 22:24:41 -0300

    Create listener.py

---
+++ listener.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import sys
+from flask import Flask, request, abort
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def webhook():
+    print("webhook"); sys.stdout.flush()
+    if request.method == 'POST':
+        print(request.json)
+        return 'OK', 200
+    else:
+        abort(400)
+
+
+if __name__ == '__main__':
+    app.run()
